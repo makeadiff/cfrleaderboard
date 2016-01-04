bda9156261fb10205d9e37b22971f055cbbe4104
Binny V A <binnyva@gmail.com>
2016-01-04 17:13:16 +0530

    Fixed an issue with database being local

--- assignment.php
+++ assignment.php
@@ -3,8 +3,8 @@ require 'common.php';
 
 
 if($_SERVER['HTTP_HOST'] == 'makeadiff.in') {
-	$sql_donut = new Sql("makeadiff_cfrapp");
-	$sql_madapp= new Sql("makeadiff_madapp");
+	$sql_donut = new Sql($config_data['db_host'], $config_data['db_user'], $config_data['db_password'], "makeadiff_cfrapp");
+	$sql_madapp= new Sql($config_data['db_host'], $config_data['db_user'], $config_data['db_password'], "makeadiff_madapp");
 } else {
 	$sql_donut = new Sql("Project_Donut");
 	$sql_madapp= new Sql("Project_Madapp");
