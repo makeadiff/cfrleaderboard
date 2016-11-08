d806f4c7dd2483ead78bad1dea097184422d7efe
Binny V A <binnyva@gmail.com>
2016-11-08 15:54:21 +0530

    Wrong DB again

--- includes/application.php
+++ includes/application.php
@@ -1,6 +1,6 @@
 <?php
 if($_SERVER['HTTP_HOST'] == 'makeadiff.in') {
-	$sql_madapp= new Sql($config_data['db_host'], $config_data['db_user'], $config_data['db_password'], "makeadiff_cfrapp");
+	$sql_madapp= new Sql($config_data['db_host'], $config_data['db_user'], $config_data['db_password'], "makeadiff_madapp");
 } else {
 	$sql_madapp= new Sql("Project_Madapp");
 }
