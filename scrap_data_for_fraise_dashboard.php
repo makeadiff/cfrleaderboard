3cbc62873420789d40925785d37b20542e03c510
Sanjay Thomas Alanoly <me@sanjayalanoly.com>
2016-12-08 12:32:15 +0530

    Correction

--- scrap_data_for_fraise_dashboard.php
+++ scrap_data_for_fraise_dashboard.php
@@ -1,11 +1,11 @@
 <?php
-/*header('Content-Type: text/csv; charset=utf-8');
-header('Content-Disposition: attachment; filename=data.csv');*/
+header('Content-Type: text/csv; charset=utf-8');
+header('Content-Disposition: attachment; filename=data.csv');
 
-/*$output = fopen('php://output', 'w');
-fputcsv($output, array('City', 'Raised'));*/
+$output = fopen('php://output', 'w');
+fputcsv($output, array('City', 'Raised'));
 
-$data = file_get_contents('http://localhost/makeadiff.in/home/makeadiff/public_html/apps/cfrleaderboard/');
+$data = file_get_contents('http://makeadiff.in/apps/cfrleaderboard/');
 $regex = '/<td width="60%" class="unit-name" name="city-name">[0-9]*.[\s]*(.+?)<\/td>[\n\r\s]+<td width="25%" name="city-raised">₹ (.+?)<\/td>/';
 preg_match_all($regex,$data,$match);
 for($i=0;$i< sizeof($match[2]);$i++) {
@@ -14,12 +14,10 @@ for($i=0;$i< sizeof($match[2]);$i++) {
 
 }
 
-/*for($i=0;$i< sizeof($match[2]);$i++) {
+for($i=0;$i< sizeof($match[2]);$i++) {
 
     fputcsv($output,array($match[1][$i],$match[2][$i]));
 
-}*/
-
-var_dump($match);
+}
 
 ?>
