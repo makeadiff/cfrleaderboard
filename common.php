7ef8fffdf5d1dbf9de99cadb4717dc3ef07ba0be
Binny V A <binnyva@gmail.com>
2017-10-07 11:28:23 +0530

    Fixed some issue with the DB Change in localsystem

--- common.php
+++ common.php
@@ -6,14 +6,9 @@ setlocale(LC_MONETARY, 'en_IN');
 $mem = new Memcached();
 $mem->addServer("127.0.0.1", 11211);
 
-$year = 2016;
+$year = 2017;
 $cache_expire = 60 * 60;
 $top_count = 30;
 
-if($_SERVER['HTTP_HOST'] == 'makeadiff.in') {
-	$db_madapp 		= 'makeadiff_madapp';
-	$db_donut 		= 'makeadiff_cfrapp';
-} else {
-	$db_madapp 		= 'Project_Madapp';
-	$db_donut 		= 'Project_Donut';
-}
+$db_madapp 		= 'makeadiff_madapp';
+$db_donut 		= 'makeadiff_cfrapp';
--- includes/application.php
+++ includes/application.php
@@ -1,10 +1,5 @@
 <?php
-if(isset($_SERVER['HTTP_HOST']) and $_SERVER['HTTP_HOST'] == 'makeadiff.in') {
-	$sql_madapp= new Sql($config_data['db_host'], $config_data['db_user'], $config_data['db_password'], "makeadiff_madapp");
-} else {
-	$sql_madapp= new Sql("Project_Madapp");
-}
-
+$sql_madapp= new Sql($config_data['db_host'], $config_data['db_user'], $config_data['db_password'], "makeadiff_madapp");
 
 $city_transilation = array(
 		// Madapp City ID 		=> Donut City ID
