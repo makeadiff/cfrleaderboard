36726f530229f640b2ba1939e81e617243818069
rhit99900 <rohitcse081@gmail.com>
2017-10-24 12:41:32 +0530

    Remove Regions V1.0

---
+++ templates/header.php
@@ -0,0 +1,15 @@
+<div class="navbar-fixed">
+  <nav>
+    <div class="nav-wrapper">
+       <a href="#" class="brand-logo center-align">&nbsp; &nbsp; CFR Leaderboard</a>
+       <!-- <ul class="nav navbar-nav">
+         <li>City Dashboard</li>
+         <li>Vetical Dashboard</li>
+       </ul> -->
+       <ul id="nav-mobile" class="right hide-on-med-and-down">
+         <li>City Dashboard&nbsp; &nbsp;</li>
+         <li>Vetical Dashboard&nbsp; &nbsp;</li>
+       </ul>
+    </div>
+  </nav>
+</div>
